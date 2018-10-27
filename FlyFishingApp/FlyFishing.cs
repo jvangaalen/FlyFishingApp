ec822e6235c7084027cbe7670075d072f5aff125
Jerome vanGaalen <jerome.vangaalen@gmail.com>
2018-10-27 13:55:59 -0400

    Started project, added properties to FlyFishing class.

---
+++ FlyFishingApp/FlyFishing.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlyFishingApp
+{
+    class FlyFishing
+    {
+        public int Id { get; set; }
+        public string Fly { get; set; }
+        public int Size { get; set; }
+        public string Month { get; set; }
+        public bool CanUseFly { get; set; }
+    }
+}
