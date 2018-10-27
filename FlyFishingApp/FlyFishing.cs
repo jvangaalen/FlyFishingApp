7b45eb28eb5c3e5ec7c28b3cc14850d4a7dfccd4
Jerome vanGaalen <jerome.vangaalen@gmail.com>
2018-10-27 15:30:30 -0400

    Deleted Fyfishing Reader class, deleted FlyFishingWriter class. made a FlyFishingReader class in FlyFishingApp.cs, added a FlyFishingResult class to FlyFishingApp.cs

--- FlyFishingApp/FlyFishing.cs
+++ FlyFishingApp/FlyFishing.cs
@@ -6,12 +6,22 @@ using System.Threading.Tasks;
 
 namespace FlyFishingApp
 {
-    class FlyFishing
+    public class FlyFishingReader
     {
         public int Id { get; set; }
         public string Fly { get; set; }
         public int Size { get; set; }
         public string Month { get; set; }
         public bool CanUseFly { get; set; }
+
+        public ICollection<FlyFishingResult> FlyFishingResults { get; set; }
     }
+
+    public class FlyFishingResult
+    {
+        public string Fly { get; set; }
+        public int Size { get; set; }
+        public string Month { get; set; }
+    }
+
 }
